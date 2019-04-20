401810a1fa9d54aee21cebb06dfba4365b9ece19
Dima T <mrlocky98@gmail.com>
2019-04-20 23:15:07 +0300

    bow creation

--- bow_classifier.py
+++ bow_classifier.py
@@ -1,22 +1,38 @@
 import nltk
+import numpy as np
+from nltk.corpus import stopwords
+
+stop_words = stopwords.words('english')
 
 def tokenize(text):
     return nltk.word_tokenize(text)
 
-def preprocess(text):
+def preprocess(text, remove_stop=True):
     tokenized = tokenize(text.lower())
-    return [w for w in tokenized if w.isalpha()]
+    if remove_stop:
+        return [w for w in tokenized if w.isalpha() and w not in stop_words]
+    else:
+        return [w for w in tokenized if w.isalpha()]
 
 def create_vocabulary(corpus):
     words = []
-    for text in corpus:
+    for text in corpus:  
         words.extend(preprocess(text))
-    
     return sorted(list(set(words)))
 
-def make_BOW(corpus):
+def make_bow(corpus):
     vocab = create_vocabulary(corpus)
-    pass
+    vectors = np.zeros((len(corpus), len(vocab)))
+    for t_id, text in enumerate(corpus):
+        words = preprocess(text)
+        bag_vector = np.zeros(len(vocab))
+        for w in words:
+            for i, word in enumerate(vocab):
+                if w == word:
+                    bag_vector[i] += 1
+        vectors[t_id] = bag_vector
+    
+    return vectors
 
-def classify_BOW(corpus):
+def classify_bow(corpus):
     pass
